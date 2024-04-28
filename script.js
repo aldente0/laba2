207abf4e4340e977c44984d7853def788a1cf793
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2024-04-28 10:04:40 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-const search = window.location.search.split('?')[1];
-const expectedParams = ['first', 'second', 'third%5B%5D'];
-const getParams = search.split('&').map(function (param) {
-    return param.split('=');
-});
-
-let count = 0;
-let subcount = 0;
-getParams.forEach(function (data) {
-    console.log(data[0]);
-    console.log(data[1]);
-    if (expectedParams.includes(data[0]) && data[1] === 'correct' && !data[0].endsWith('%5B%5D')) {
-        count++;
-    } else if (expectedParams.includes(data[0]) && data[1] === 'correct' && data[0].endsWith('%5B%5D')) {
-        subcount++;
-    }
-
-});
-
-if (subcount === 2) {
-    count += 1;
-}
-
 const result = document.getElementById('result');
+const form = document.forms.test;
 
-if (count === 3) {
-    result.textContent = 'Тест завершен. Хорошие знания!';
-    result.style.color = 'green';
-} else if (count === 2) {
-    result.textContent = 'Тест завершен. Средние знания!';
-    result.style.color = 'yellow';
-} else if (count < 2) {
-    result.textContent = 'Тест завершен. Плохие знания!';
-    result.style.color = 'red';
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const expectedData = [
+        ['first', ['correct']],
+        ['second', ['correct']],
+        ['third[]', ['correct', 'correct']]
+    ];
+    let formData = new FormData(form);
+    const inputNames = formData.keys();
+    let count = 0;
+    let checked = [];
+
+    inputNames.forEach(function (name) {
+        if (checked.includes(name)) {
+            return;
+        }
+        checked.push(name);
+        const inputData = formData.getAll(name);
+        expectedData.forEach(expectedElement => {
+            if (expectedElement[0] === name && isArrayEquals(expectedElement[1], inputData)) {
+                count++;
+            }
+        });
+    });
+    if (count === 3) {
+        result.textContent = 'Тест завершен. Хорошие знания!';
+        result.style.color = 'green';
+    } else if (count === 2) {
+        result.textContent = 'Тест завершен. Средние знания!';
+        result.style.color = 'yellow';
+    } else if (count < 2) {
+        result.textContent = 'Тест завершен. Плохие знания!';
+        result.style.color = 'red';
+    }
+});
+
+function isArrayEquals(array1, array2) {
+    return array1.length === array2.length && array1.every((element, index) => element === array2[index]);
 }
 
